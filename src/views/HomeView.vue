e616edbea627ef0cf8104247801dbe6e487030bf
Asjad Ali <asjadaliwatto@gmail.com>
2023-06-07 17:53:13 +0500

    structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    Home Page
-  </div>
+  
+   <HeaderVue />
+
+   <div class="p-4">
+      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+         <div class="grid grid-cols-2 gap-2 mb-4">
+            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+               <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            </div>
+            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+               <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            </div>
+         </div>
+         <div class="grid grid-cols-2 gap-2 mb-4">
+            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+               <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            </div>
+            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+               <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            </div>
+         </div>
+         <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+         </div>
+      </div>
+   </div>
+
 </template>
+
+<script setup>
+import HeaderVue from "../components/Header.vue";
+
+</script>
